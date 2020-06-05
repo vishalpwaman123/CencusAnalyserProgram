a8f864c03923e62c1781b6c4ed82f483f7ddde55
unknown <vishalpwaman123@gmail.com>
2020-06-05 00:37:30 -0700

    Added: load indian states information from a csv file.

--- CencusAnalyserTest/UnitTest1.cs
+++ CencusAnalyserTest/UnitTest1.cs
@@ -4,11 +4,6 @@ namespace CencusAnalyserTest
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public void Test1()
         {
