51d850515c55d40dcb86e00ecb4acd758cbc425a
unknown <vishalpwaman123@gmail.com>
2020-06-04 20:34:29 -0700

    Welcome to Cencus Analyser

---
+++ CencusAnalyserProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CencusAnalyserProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cencus Analyser");
+        }
+    }
+}
