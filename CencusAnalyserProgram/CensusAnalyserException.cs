3d397da85dd04a95de7e8bd3b066d778c06218b9
unknown <vishalpwaman123@gmail.com>
2020-06-06 03:07:40 -0700

    Added : Added: Load the Indian Census File and returns number of Records in file

---
+++ CencusAnalyserProgram/CensusAnalyserException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CencusAnalyserProgram
+{
+    class CensusAnalyserException : Exception
+    {
+        public enum ExceptionType
+        {
+            CENSUS_FILE_PROBLEM
+        }
+
+        ExceptionType type;
+
+        public CensusAnalyserException(String message, ExceptionType type) : base(message)
+        {
+            this.type = type;
+        }
+
+
+    }
+}
--- CencusAnalyserTest/UnitTest1.cs
+++ CencusAnalyserTest/UnitTest1.cs
@@ -1,13 +1,25 @@
 using NUnit.Framework;
+using CencusAnalyserProgram;
+using System.Data;
 
 namespace CencusAnalyserTest
 {
     public class Tests
     {
+
+        private static string INDIA_CENSUS_CSV_FILE_PATH = @"C:/Users/Vishal/source/repos/CencusAnalyserProgram/CencusAnalyserTest/Resources/IndiaStateCensusData.csv";
+        private static  string WRONG_CSV_FILE_PATH = @"./src/main/resources/IndiaStateCensusData.csv";
+
+
         [Test]
-        public void Test1()
+        public void givenIndianCensusCSVFileReturnsCorrectRecords()
         {
-            Assert.Pass();
+           
+                CensusAnalyser censusAnalyser = new CensusAnalyser();
+                DataTable csvData = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+                Assert.AreEqual(29, csvData.Rows.Count);
+            
         }
+
     }
 }
