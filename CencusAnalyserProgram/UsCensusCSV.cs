345f3c9d84424eb7640078ab76407bcde378f31a
unknown <vishalpwaman123@gmail.com>
2020-06-12 22:47:12 -0700

    Added:Load the USCensusData and Check Number Of Records

---
+++ CencusAnalyserProgram/UsCensusCSV.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CencusAnalyserProgram
+{
+    //State_Id	State	Population	Housing_units	Total_area	Water_area	Land_area	Population_Density	Housing_Density
+
+    public class UsCencusCSV
+    {
+        public string stateId { get; set; }
+        public string state { get; set; }
+        public Int32 population { get; set; }
+        public double totalArea { get; set; }
+        public double populationDencity { get; set; }
+
+    }
+}
